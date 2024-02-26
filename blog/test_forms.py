75a69fdff9c1c64e97a133a8a460431e0e6dd445
William Stocks <willstocks@me.com>
2024-02-26 16:17:20 +0000

    Update

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -3,6 +3,11 @@ from .forms import CommentForm
 
 class TestCommentForm(TestCase):
 
-    def test_form_is_vslid(self):
+    def test_form_is_valid(self):
         comment_form = CommentForm({'body': 'This is a great post'})
-        self.assertTrue(comment_form.is_valid())
+        self.assertTrue(comment_form.is_valid(), msg='Form is invalid')
+
+    
+    def test_form_is_invalid(self):
+        comment_form = CommentForm({'body': ''})
+        self.assertFalse(comment_form.is_valid(), msg='Form is valid')
